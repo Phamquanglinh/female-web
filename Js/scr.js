08836293dceb99273bb5eba6dede7aec0fed26e2
Phamquanglinh <Linh2002gv@gmail.com>
2020-12-29 16:57:28 +0700

    Add menu bar

--- Js/scr.js
+++ Js/scr.js
@@ -1,5 +1,19 @@
 
-    function scrCart() {
-        var elmnt = document.getElementById("cart");
-        elmnt.scrollIntoView();
+    function openMenu() {
+        document.getElementById("dropmenu").style.display="block";
+        document.getElementById("action").innerHTML=
+            '<div class="collspan-btn float-right open" id="close-btn" onclick="closeMenu()">\n' +
+            '                    <span></span>\n' +
+            '                    <span></span>\n' +
+            '                    <span></span>\n' +
+            '            </div>';
+    }
+    function closeMenu() {
+        document.getElementById("dropmenu").style.display="none";
+        document.getElementById("action").innerHTML=
+            '<div class="collspan-btn float-right" id="close-btn" onclick="openMenu()">\n' +
+            '                    <span></span>\n' +
+            '                    <span></span>\n' +
+            '                    <span></span>\n' +
+            '            </div>';
     }
